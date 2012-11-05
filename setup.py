7b2a81536977bb13b68ada3e5a281e35b24e0bfd
Radek Czajka <radoslaw.czajka@nowoczesnapolska.org.pl>
2012-11-05 11:06:31 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+import os.path
+from setuptools import setup, find_packages
+
+def whole_trees(package_dir, paths):
+    def whole_tree(prefix, path):
+        files = []
+        for f in (f for f in os.listdir(os.path.join(prefix, path)) if not f[0]=='.'):
+            new_path = os.path.join(path, f)
+            if os.path.isdir(os.path.join(prefix, new_path)):
+                files.extend(whole_tree(prefix, new_path))
+            else:
+                files.append(new_path)
+        return files
+    prefix = os.path.join(os.path.dirname(__file__), package_dir)
+    files = []
+    for path in paths:
+        files.extend(whole_tree(prefix, path))
+    return files
+
+setup(
+    name='fnpdjango',
+    version='0.1',
+    author='Radek Czajka',
+    author_email='radoslaw.czajka@nowoczesnapolska.org.pl',
+    url = '',
+    packages=find_packages(),
+    package_data={'fnpdjango': whole_trees('fnpdjango', 
+            ['templates', 'locale'])},
+    license='LICENSE',
+    description='.',
+    long_description="",
+)
