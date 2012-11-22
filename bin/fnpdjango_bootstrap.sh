deab1162c0d65293acfdc723ad9f6676f653f2fb
Radek Czajka <radoslaw.czajka@nowoczesnapolska.org.pl>
2012-11-22 11:03:58 +0100

    fix bootstrap script

--- bin/fnpdjango_bootstrap.sh
+++ bin/fnpdjango_bootstrap.sh
@@ -7,4 +7,6 @@ pip install "$DJANGO_REQ"
 django-admin.py startproject \
     --template http://pypi.nowoczesnapolska.org.pl/bootstrap/project.tar.gz \
     "$1"
+chmod +x "$1"/manage.py
+pip install -r "$1"/requirements.txt
 pip install -r "$1"/requirements-dev.txt
