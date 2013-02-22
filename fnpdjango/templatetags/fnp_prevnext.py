28fac547f6c8639b4c4edd66c895dacc7dff030e
Radek Czajka <radoslaw.czajka@nowoczesnapolska.org.pl>
2013-02-22 13:34:29 +0100

    minor fix

--- fnpdjango/templatetags/fnp_prevnext.py
+++ fnpdjango/templatetags/fnp_prevnext.py
@@ -11,7 +11,6 @@ def previous_page(context, fallback=None, fallback_title=None):
     if current > 1:
         get_dict = copy(context['request'].GET)
         get_dict['page'] = current - 1
-        print get_dict
         return {'number': current - 1, 'title': None, 'url': None,
                 'get_dict': urlencode(get_dict)}
     else:
@@ -25,7 +24,6 @@ def next_page(context, fallback=None, fallback_title=None):
     if current < page_range[-1]:
         get_dict = copy(context['request'].GET)
         get_dict['page'] = current + 1
-        print get_dict
         return {'number': current + 1, 'title': None, 'url': None,
                 'get_dict': urlencode(get_dict)}
     else:
