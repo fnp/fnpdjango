2c0647828496c7efb9edcf7ae8d5b392c195cd58
Radek Czajka <radekczajka@nowoczesnapolska.org.pl>
2014-03-27 15:23:21 +0100

    Use fnpdjango.auth_backends.AttrCASBackend.

--- src/project_name/settings/auth.py
+++ src/project_name/settings/auth.py
@@ -4,5 +4,5 @@ from . import INSTALLED_APPS
 if 'django_cas' in INSTALLED_APPS:
     AUTHENTICATION_BACKENDS = (
         'django.contrib.auth.backends.ModelBackend',
-        'django_cas.backends.CASBackend',
+        'fnpdjango.auth_backends.AttrCASBackend',
     )
