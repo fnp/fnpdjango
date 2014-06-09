26e2e8f9044a3d72cdc66be32cbf62aed118b146
Aleksander Łukasz <aleksander.lukasz@nowoczesnapolska.org.pl>
2014-06-09 14:27:34 +0200

    AttrCASBackend: Save user instance only when necessary

--- fnpdjango/auth_backends.py
+++ fnpdjango/auth_backends.py
@@ -10,13 +10,17 @@ attr_map = getattr(settings, 'CAS_USER_ATTRS_MAP', {
 class AttrCASBackend(CASBackend):
     def authenticate(self, ticket, service, request):
         user = super(AttrCASBackend, self).authenticate(ticket, service, request)
+        dirty = False
         for attr, value in request.session.get('attributes', {}).items():
             try:
                 local_attr = attr_map[attr]
             except KeyError:
                 pass
             else:
-                setattr(user, local_attr, value)
-        user.save()
+                if getattr(user, local_attr, None) != value:
+                    setattr(user, local_attr, value)
+                    dirty = True
+        if dirty:
+            user.save()
         return user
 
