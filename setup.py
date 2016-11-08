6dda756df845b4811aad6dfc918e6929fd3b8ce5
Jan Szejko <janek37@gmail.com>
2016-11-08 14:48:37 +0100

    version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def whole_trees(package_dir, paths):
 
 setup(
     name='fnpdjango',
-    version='0.2.1',
+    version='0.2.2',
     author='Radek Czajka',
     author_email='radekczajka@nowoczesnapolska.org.pl',
     url = '',
