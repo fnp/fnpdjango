3b009e3e675620c33423c624985726ee8db94e86
Jan Szejko <janek37@gmail.com>
2017-07-25 11:49:06 +0200

    TextileField

---
+++ fnpdjango/utils/fields.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from django.forms import CharField, ValidationError
+from .text.textilepl import textile_pl
+
+try:
+    from markupfield.fields import MarkupField
+except ImportError:
+    MarkupField = None
+else:
+    def validate_textile(value):
+        try:
+            textile_pl(value)
+        except Exception:
+            raise ValidationError('Syntax error in markup.')
+
+
+    class TextileFormField(CharField):
+        default_validators = [validate_textile]
+
+
+    class TextileField(MarkupField):
+        def formfield(self, **kwargs):
+            defaults = {'form_class': TextileFormField}
+            defaults.update(kwargs)
+            return super(MarkupField, self).formfield(**defaults)
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def whole_trees(package_dir, paths):
 
 setup(
     name='fnpdjango',
-    version='0.2.4',
+    version='0.2.6',
     author='Radek Czajka',
     author_email='radekczajka@nowoczesnapolska.org.pl',
     url='',
